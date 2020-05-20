62980e7d5eeaee5392364ac8a2b0cdac8875131a
Marvin <marvin.trossingeneb.de>
2020-05-20 16:54:45 +0200

    Use Java style argument array and fix imports

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,8 @@
-import java.lang.*;
 import java.util.*;
 /**
  * Note that Java console applications need to be run through the java runtime
  * by running "java -jar JarFile.jar" in the command line.
- * Java console applications can not be previewed in the Compilr IDE, only applets can.
+ * Java console applications can not be previewed in the Compiler IDE, only applets can.
  */
 public class Program
 {
@@ -12,7 +11,7 @@ public class Program
      */
     
     
-    public static void main(String args[]) 
+    public static void main(String[] args)
     {
         String result;
         System.out.println("Welcome to the Movie Store");
